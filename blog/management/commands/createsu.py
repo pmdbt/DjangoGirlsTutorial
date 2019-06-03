1a380ad8a09b226e07ca0f5dde5c827479dbc91f
Jerry Chu <jerry@lofty.ai>
2019-06-03 11:56:31 -0700

    fixing indentation error in createsu.py file

--- blog/management/commands/createsu.py
+++ blog/management/commands/createsu.py
@@ -4,5 +4,5 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
